32238c27f710f0b0ec76ea0fe203f37d4622ff68
water1bottle1 <148641636+water1bottle1@users.noreply.github.com>
2026-02-11 19:30:38 +0000

    added users and password

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    Lilwhitetwinky20: "62052754",
+    giggitizer: "82748263",
+    Ryans: "73952482",
+    bigblackstudent: "12847384",
+    guestuser1: "password1",
   },
 };
 
